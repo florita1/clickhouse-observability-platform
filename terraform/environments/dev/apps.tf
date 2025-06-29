f1e146d15ef06e028843e1d64c9a1475a3f5c439
florita1 <floritaggie@gmail.com>
2025-06-28 22:49:50 -0500

    add ingestion service helm chart

--- terraform/environments/dev/apps.tf
+++ terraform/environments/dev/apps.tf
@@ -1,3 +1,7 @@
 resource "kubernetes_manifest" "clickhouse_app" {
   manifest = yamldecode(file("${path.module}/../../../apps/clickhouse.yaml"))
 }
+
+resource "kubernetes_manifest" "ingestion_service_app" {
+  manifest = yamldecode(file("${path.module}/../../../apps/ingestion-service.yaml"))
+}
