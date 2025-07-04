c1174d1107c115760ce3597bb26dc44ad1c5307a
florita1 <floritaggie@gmail.com>
2025-07-04 00:05:22 -0500

    add clickhouse secret back in

--- terraform/environments/dev/apps.tf
+++ terraform/environments/dev/apps.tf
@@ -5,3 +5,16 @@ resource "kubernetes_manifest" "clickhouse_app" {
 resource "kubernetes_manifest" "ingestion_service_app" {
   manifest = yamldecode(file("${path.module}/../../../apps/ingestion-service.yaml"))
 }
+
+resource "kubernetes_secret" "clickhouse" {
+  metadata {
+    name      = "clickhouse-secret"
+    namespace = "ingestion"
+  }
+
+  data = {
+    password = var.clickhouse_password
+  }
+
+  type = "Opaque"
+}
